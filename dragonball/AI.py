b8d62f0471113e7a9ffe8442d08a858c1d582f03
Ssukdaebat60 <84669895+Ssukdaebat60@users.noreply.github.com>
2021-12-03 10:11:30 +0900

    Update AI.py

--- dragonball/AI.py
+++ dragonball/AI.py
@@ -7,18 +7,21 @@ class AI:
         self.skillList = skillList
         self.randomList = [
                 [90, 10, 0, 0, 0, 0],
-                [40, 20, 70, 50, 0, 0],
-                [10, 20, 90, 0, 0, 0],
+                [45, 20, 70, 50, 0, 0],
+                [90, 20, 50, 50],
+                [10, 20, 0, 0, 100, 0],
             ]
         
     def AIChoice(self, energy): #random을 사용할 때는  어떻게 테스트 해야 하나요?
         index = 0
         if energy == 0:
             index = self.randomChoice(self.randomList[0])
-        elif energy >= 1:
+        elif energy == 1:
             index = self.randomChoice(self.randomList[1])
-        elif energy >= 5 :
+        elif energy >= 2 and energy < 5:
             index = self.randomChoice(self.randomList[2])
+        elif energy >= 5 :
+            index = self.randomChoice(self.randomList[3])
         return self.skillList[index]
     
     def randomChoice(self, RList):
